cee36874270397d9b1026a3e3bdfaf4efc010c32
tinjet_11 <tinjet1016@gmail.com>
2023-06-28 16:05:22 +0800

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,10 @@ import AuthProviders from './AuthProviders';
 import Link from 'next/link';
 import Image from "next/image";
 import { NavLinks } from '@/constants';
+import { getCurrentUser } from '@/lib/session';
 
-const Navbar = () => {
-  const session = null;
+const  Navbar = async () => {
+  const session = await getCurrentUser() ;
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flexStart gap-10'>
@@ -27,9 +28,18 @@ const Navbar = () => {
       </div>
 
       <div className='flexCenter gap-4'>
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+          {session?.user?.image &&(
+            <Image 
+            src={session.user.image} 
+            alt={session.user.name} 
+            width={40}
+            height={40}
+            className='rounded-full'
+            />)
+          }
+          
             <Link href="/create-project">
               share work
             </Link>
