7b746998b7eee95300e8d804462e07ae0f01cea7
tinjet_11 <tinjet1016@gmail.com>
2023-06-28 13:49:28 +0800

    added database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
     title: "Flexibble",
@@ -10,11 +11,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang='en'>
             <body>
-
+                <Navbar />
                 <main>
                     {children}
                 </main>
-
+                <Footer />
             </body>
         </html>
     );
---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import AuthProviders from './AuthProviders';
+import Link from 'next/link';
+import Image from "next/image";
+import { NavLinks } from '@/constants';
+
+const Navbar = () => {
+  const session = null;
+  return (
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            width={116}
+            height={43}
+            alt='logo'
+          />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className='flexCenter gap-4'>
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">
+              share work
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
